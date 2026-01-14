7a970a67d12ee795ef413d3ede7f8e62f81b6423
Hpayeur <hailpaye017@gmail.com>
2026-01-14 16:54:05 -0700

    setting up js codes

--- cards.js
+++ cards.js
@@ -3,3 +3,7 @@ let cards = [];
 
 // look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
 // Look up how to push an object to an array
+cards.map((item) => {
+
+})
+${card.title}
