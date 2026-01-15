25bc9a8082f93014c59272999992c4e0c9f90ffb
Hpayeur <hailpaye017@gmail.com>
2026-01-14 17:31:37 -0700

    testing out Different codes

--- cards.js
+++ cards.js
@@ -1,9 +1,28 @@
-let cardsList = document.querySelector(".cards");
-let cards = [];
+// let cardsList = document.querySelector(".cards");
+// let cards = [];
+
+// const cardData = [
+//   { title: "Card 1", content: "card 1 content" },
+//   { title: "Card 2", content: "card 2 content" },
+//   { title: "Card 3", content: "card 3 content" },
+//   { title: "Card 4", content: "card 4 content" },
+//   { title: "Card 5", content: "card 5 content" },
+// ];
+
+// cards.push(...cardData);
+
+// cards.forEach((card) => {
+//   const el = document.createElement("div");
+//   el.className = "card";
+//   el.innerHTML = `<h3>${card.title}</h3><p>${card.content}</p>`;
+//   if (cardsList) cardsList.appendChild(el);
+// });
+
+// let cardsList = document.querySelector(".cards");
+// let cards = [];
+// cards.map((item) => {})
+// ${card.title}
 
 // look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
 // Look up how to push an object to an array
-cards.map((item) => {
-
-})
-${card.title}
+// Example objects use { key: value } and arrays are comma-separated lists of values
