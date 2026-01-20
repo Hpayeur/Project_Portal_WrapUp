fd636934d4aa69340e1fc9eac1c0497e59aa8cb3
Hpayeur <hailpaye017@gmail.com>
2026-01-20 14:13:49 -0700

    Setting Everything Up

---
+++ cards.js
@@ -0,0 +1,43 @@
+const projects =[
+    {
+        title:"Weather News App",
+        description:"",
+        imageURl:"",
+        projectLink:""
+    }
+{      title:"",
+        description:"Kitten News App",
+        imageURl:"",
+        projectLink:""
+    }
+    {      title:"Wolf Club Project",
+        description:"",
+        imageURl:"",
+        projectLink:""
+    }
+        {      title:"Memory Game Project",
+        description:"",
+        imageURl:"",
+        projectLink:""
+    }
+        {      title:"Find My Restaurant",
+        description:"",
+        imageURl:"",
+        projectLink:""
+    }
+        {      title:"Mario Club Project",
+        description:"",
+        imageURl:"",
+        projectLink:""
+    }
+]
+
+
+// let cardsList = document.querySelector(".cards");
+// let cards = [];
+// cards.map((item) => {
+// })
+// ${card.title}
+// look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
+// Look up how to push an object to an array
+// write the following code to start creating your map:
